154feedf42b7d71f8686fa24a5c7b92492c0dd39
Marta <turkovska05@gmail.com>
2022-12-14 20:02:24 +0200

    yeaaaah

---
+++ UnitTest6.2rek/UnitTest6.2rek.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6.2rek/lab_6.2rek.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62rek
+{
+	TEST_CLASS(UnitTest62rek)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[5];
+			Create(a, 5, -10, 10, 0);
+			Assert::AreEqual(a[0], 10, 0);
+
+		}
+	};
+}
